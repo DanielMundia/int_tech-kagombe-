e930971d7c03a9c0ff8bb29312328aeb9d7576a8
DanielMundia <dkkamau396@gmail.com>
2026-01-21 11:42:14 +0300

    modified js to add marquee

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const output = document.querySelector("#output");
 registrationForm.addEventListener("submit", function(e){
     e.preventDefault();
     output.innerHTML = `<h3>Student Details</h3>
-    <p><strong>Name:</strong> ${name.value}</p>
-    <p><strong>Age:</strong> ${age.value}</p>
-    <p><strong>Registration Number:</strong> ${password.value}</p>
+    <marquee behavior="slide" direction="" scrollamount="8">
+        <p><span>Name:</span> ${name.value}</p>
+        <p><strong>Age:</strong> ${age.value}</p>
+        <p><strong>Registration Number:</strong> ${password.value}</p>
+    </marquee>
     `;
 });
